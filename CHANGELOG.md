cb2b3b105dc46097e23af0d97d9bf505d83b8e96
olehstrokan <oleh@wattstor.com>
2022-01-18 19:46:26 +0100

    v0.0.4. Connect the database, prod/dev .evn

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,12 @@
 
 1. Delete unnecessary files
 2. Preparation code structure for development
+
+# v0.0.4 (18.01.2022)
+
+## Features:
+
+1. Connect database
+2. New .evn for production and development
+3. Add all necessary libraries
+
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
-  imports: [],
   controllers: [],
   providers: [],
+  imports:  [
+  ConfigModule.forRoot({
+    envFilePath: `.${process.env.NODE_ENV}.env`
+  }),
+  ServeStaticModule.forRoot({
+    rootPath: path.resolve(__dirname, 'static')
+  }),
+  SequelizeModule.forRoot({
+    dialect: 'postgres',
+    host: process.env.POSTGRES_HOST ,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.root,
+    database: process.env.POSTGRES_DB,
+    models: [],
+    autoLoadModels: true
+  })],
 })
 export class AppModule {}
